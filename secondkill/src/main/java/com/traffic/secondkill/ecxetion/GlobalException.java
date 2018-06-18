f3d45c691428de615d5cd1e7aa1f7af962c63977
traffic_lee <leemcong@163.com>
2018-06-18 17:22:01 +0800

    add thymeleaf module

---
+++ secondkill/src/main/java/com/traffic/secondkill/ecxetion/GlobalException.java
@@ -0,0 +1,19 @@
+package com.traffic.secondkill.ecxetion;
+
+import com.traffic.secondkill.result.CodeMsg;
+
+public class GlobalException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	private CodeMsg cm;
+
+	public GlobalException(CodeMsg cm) {
+		super(cm.toString());
+		this.cm = cm;
+	}
+
+	public CodeMsg getCm() {
+		return cm;
+	}
+}
